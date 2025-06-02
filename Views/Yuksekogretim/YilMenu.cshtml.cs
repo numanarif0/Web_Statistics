f8cf0e78048ec1b12a340a812ca24b402b8db7d8
Eyup Gurer <mr.gurer1@gmail.com>
2025-06-02 11:06:58 +0300

    Yıl menü düzenlendi

---
+++ Views/Yuksekogretim/YilMenu.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YokIstatistikWeb.Views.Yuksekogretim;
+
+public class YilMenu : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
